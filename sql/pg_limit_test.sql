91dbf093473425740ed263cd2f4d1c905433ab1a
David Christensen <david.christensen@crunchydata.com>
2021-09-02 17:24:23 -0500

    test: coverage of all interface; explain what/why for each

--- sql/pg_limit_test.sql
+++ sql/pg_limit_test.sql
@@ -1,5 +1,26 @@
+-- basic sanity check that we show all rows
 SELECT generate_series(1,10);
+
+-- test basic max_rows functionality
 SET pg_limit.max_rows = 5;
+
+-- less than threshold; no notify
+SELECT generate_series(1,4);
+
+-- boundary condition; no notify
+SELECT generate_series(1,5);
+
+-- this should be truncated to 5 rows; notify
 SELECT generate_series(1,10);
+
+-- now check the notify_total_count flag handling
 SET pg_limit.notify_total_count = true;
+
+-- less than threshold; no notify
+SELECT generate_series(1,4);
+
+-- boundary condition; no notify
+SELECT generate_series(1,5);
+
+-- this should be truncated to 5 rows; notify with total
 SELECT generate_series(1,10);
